cb38685071e6ad90d7ae020e9dec105abd6a0732
davidhitman <hitimanadav47@gmail.com>
2023-03-14 02:02:13 +0000

    ch

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const {MongoClient, ObjectId} = require("mongodb");
-//var cors = require("cors");
+
 
 const uri = 'mongodb+srv://David:David.123@webapp.kozhplr.mongodb.net/?retryWrites=true&w=majority';
 const client = new MongoClient(uri);
@@ -15,45 +15,11 @@ async function connect(){
 }
 connect();
 
-// definning all fuction that will be in contact with mongodb and
-//make all the transactions
-
-// adding to the order table
-async function createOrder(order) {
-	return await client.db("WebStore").collection("order").insertOne(order);
-}
-
-// get the lessons from mongodb
-async function getLessons() {
-	    return client
-    .db("WebStore")
-    .collection("products")
-    .find().toArray();
-}
-
-// update lesson space in mongodb
-async function updateLesson(id, space) {
-	    return await client
-    .db("WebStore")
-    .collection("products")
-    .updateOne({ _id: ObjectId(id) }, { $inc: { "spaces": -space } });
-}
-
-// search for items in mongodb
-async function searchLesson(searchTerm) {
-        return client
-    .db("WebStore")
-    .collection("Products")
-    .find({
-        topic: { $regex: searchTerm, $options: "is" },
-    })
-    .toArray();
-}
-
 // setting up express server
 const express = require("express");
 const app = express();
 app.use(express.json());
+//var cors = require("cors");
 //app.use(cors()); 
 
 // port where the app is listenning
